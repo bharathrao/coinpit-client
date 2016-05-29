067c41088f52b85ba12a09c577c20190ff0bf8a0
Nirmal Gupta <nirmalk.gupta@gmail.com>
2016-05-29 00:48:25 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 module.exports = function (coinpitUrl) {
   var socket    = require("socket.io-client")(coinpitUrl, { rejectUnauthorized: true })
-  var network   = coinpitUrl.indexOf("live.coinpit.io") !== -1 ? "bitcoin" : "testnet"
-
-  var loginless = require("loginless")(coinpitUrl, "/api/auth/", network, console.log.bind(console))
+  var rest = require('rest.js')
   var account = require("./account")
   var client  = {}
 
   client.getAccount = function (privKey) {
-    return loginless.getServerKey(privKey).then(function (serverResponse) {
-      loginless.socket.register(socket)
-      return account(serverResponse, loginless, socket)
+    return rest.get(baseurl + '/api/config').then(function(config){
+      var loginless = require("loginless")(coinpitUrl, "/api/auth/", config.network, console.log.bind(console))
+      return loginless.getServerKey(privKey).then(function (serverResponse) {
+        loginless.socket.register(socket)
+        return account(serverResponse, loginless, socket)
+      })
     })
   }
 
